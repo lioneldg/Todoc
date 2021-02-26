2dd326465b6704b02b7c2d741d05397ee136a6bb
ldegans <lionel.degans@shadline.com>
2021-02-26 15:12:11 +0100

    TaskDAO written

--- app/src/main/java/com/cleanup/todoc/room/ProjectDao.java
+++ app/src/main/java/com/cleanup/todoc/room/ProjectDao.java
@@ -3,19 +3,10 @@ package com.cleanup.todoc.room;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 @Dao
 public interface ProjectDao {
-   /* @Query("SELECT * FROM user")
-    List<User> getAll();
-    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-    List<User> loadAllByIds(int[] userIds);
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    User findByName(String first, String last);
-    @Insert
-    void insertAll(User... users);
-    @Delete
-    void delete(User user);*/
+
 }
--- app/src/main/java/com/cleanup/todoc/room/TaskDao.java
+++ app/src/main/java/com/cleanup/todoc/room/TaskDao.java
@@ -3,19 +3,25 @@ package com.cleanup.todoc.room;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
+
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
 
 @Dao
 public interface TaskDao {
-   /* @Query("SELECT * FROM user")
-    List<User> getAll();
-    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-    List<User> loadAllByIds(int[] userIds);
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    User findByName(String first, String last);
-    @Insert
-    void insertAll(User... users);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void addTasks(Task... tasks);
+
+    @Query("SELECT * FROM task WHERE projectId IN (:projectId)")
+    List<Task> tasksByProjectId(long projectId);
+
+    @Update
+    public void updateTasks(Task... tasks);
+
     @Delete
-    void delete(User user);*/
+    public void deleteTasks(Task... tasks);
 }
