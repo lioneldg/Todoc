40537211e800295550454e6c64cb7b4896d30d41
lioneldg <lioneldegans@gmail.com>
2021-03-12 16:52:51 +0100

    clean project

--- app/src/main/java/com/cleanup/todoc/database/ProjectDao.java
+++ app/src/main/java/com/cleanup/todoc/database/ProjectDao.java
@@ -2,25 +2,15 @@ package com.cleanup.todoc.database;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
-import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
-
 import com.cleanup.todoc.model.Project;
-
 import java.util.List;
 
 @Dao
 public interface ProjectDao {
 
-    // CREATE
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void addProjects(Project... projects);
-
     // READ
     @Query("SELECT * FROM project")
     LiveData<List<Project>> getAllProjects();
 
-    @Query("SELECT * FROM project WHERE id = :projectId")
-    LiveData<Project> getProjectById(long projectId);
 }
